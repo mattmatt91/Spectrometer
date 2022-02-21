ef593e864e4398a256bf4e04d54838d2c65a744f
mattmatt91 <matthias_muhr@web.de>
2022-02-21 16:12:20 +0100

    update

--- main.py
+++ main.py
@@ -4,32 +4,33 @@ from seabreeze.spectrometers import Spectrometer
 import pandas as pd
 from matplotlib import pyplot as plt
 from time import time_ns
+import plotly.graph_objects as go
+
 
 start_time = time_ns()
 
 def get_time():
-    return (time_ns() - start_time)/1000000
-
+    return (time_ns() - start_time)/1000000000
 
 
 ammount_spectras = 100
 spec = Spectrometer.from_first_available()
-
 spec.integration_time_micros(20000)
 counts = []
 timestamps = []
+
 for i in range(ammount_spectras):
     counts.append(spec.intensities())
     timestamps.append(get_time())
 
-df = pd.DataFrame({'wavelength': spec.wavelengths()})
-for spectra, timestamp in zip(counts, timestamps):
-    df[timestamp] = spectra.tolist()
-    
-print(df.head())
 
-for i in range(len(timestamps)-1):
-    print(timestamps[i+1]- timestamps[i])
-df.set_index('wavelength', inplace=True)
-df.plot()
-plt.show()
+fig = go.Figure(data=[go.Surface(z=counts, y=timestamps, x=spec.wavelengths())])
+fig.update_layout(title='spectra', autosize=True,
+                  width=800, height=800, scene = dict(
+                    xaxis_title='wavelength [nm]',
+                    yaxis_title='time [s]',
+                    zaxis_title='counts'))
+            
+fig.show()
+
+
